799151e71cca096ad949393c2efbb22247c9c6a1
Alberto Taiuti <alberto.taiuti@gmail.com>
2017-09-17 10:05:01 +0100

    Impl CIFAR-10 batch1 load & PNG output of cat imgs to test loading

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # catpicker-rs
 Machine Learning experiments in Rust.
+
+## Getting started
+
+### Prerequisites
+* [Rust](https://www.rustup.rs/)
+* [Cargo](https://crates.io/)
+* [CIFAR-10 datasets](https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz)
+
+### Building
+Download the CIFAR-10 bin datasets and extract them in the root folder
+of the project, i.e:
+```
+/project-root-dir/
+-- /cifar-10-batches-bin/
+---- /data_batch_1.bin
+---- .... etc
+```
+
+Use Cargo to build and run so that the dependencies are staisfied.
+
+## References
+[Deep Learning on Coursera](https://www.coursera.org/learn/neural-networks-deep-learning/)
+
+## Authors
+* **Alberto Taiuti** - *Developer* -
+[@snowzurfer](https://github.com/snowzurfer)
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file
+for details
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+extern crate nalgebra as na;
+extern crate png;
+
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::path::Path;
+use std::io::BufWriter;
+// To use encoder.set()
+use png::HasParameters;
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("cifar-10-batches-bin/data_batch_1.bin").unwrap();
+    let mut reader = BufReader::new(f);
+
+    let cfar_m = 10000usize;
+    let cfar_n = 3072usize;
+    let cfar_img_byte_stride = 3073usize;
+    let mut X: na::DMatrix<u8> = na::DMatrix::identity(cfar_n, cfar_m);
+    let mut Y: na::DVector<u8> = na::DVector::identity(cfar_m);
+
+    let multiplier = cfar_img_byte_stride;
+    for i in 0..cfar_m {
+        // Read first byte which tells us the category of the image
+        let mut label = [0u8];
+        let read = reader.read(&mut label).unwrap();
+        assert_eq!(read, 1);
+
+        // If cat, set it as one
+        if label[0] == 3 {
+            Y[i] = 1;
+            println!("We are at i = {}", i);
+        }
+        // If not, set it as zero
+        else {
+            Y[i] = 0;
+        }
+
+        // Read the data of an image into a column of the data vector
+        let mut img_data = vec![0; cfar_n];
+        reader.read_exact(&mut img_data).unwrap();
+
+        if label[0] == 3 {
+            let path = Path::new(r"image.png");
+            let file = File::create(path).unwrap();
+            let ref mut w = BufWriter::new(file);
+
+            let mut encoder = png::Encoder::new(w, 32, 32); // Width is 2 pixels and height is 1.
+            encoder.set(png::ColorType::RGB).set(png::BitDepth::Eight);
+            let mut writer = encoder.write_header().unwrap();
+
+            let mut data = vec![0u8; cfar_n];
+            for n in 0..1024 {
+                let nn = n * 3;
+                data[nn] = img_data[n];
+                data[nn + 1] = img_data[n + 1024];
+                data[nn + 2] = img_data[n + 2048];
+            }
+
+            writer.write_image_data(&data).unwrap(); // Save
+        }
+
+    }
+
+    //println!("Y content is: {}", Y);
+
 }
