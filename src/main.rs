4e290abb05741048888aac1ff9be1946c3d3d386
Alberto Taiuti <alberto.taiuti@gmail.com>
2017-09-24 15:13:38 +0100

    Impl logistic regression for CIFAR-10, data batch 1
    Implement first version of logistic regression over the first data batch of the
    CIFAR-10 data set.
    
    Write the whole program is in `main.rs`. In the future the code will
    be factored out into structs, traits and modules.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io;
 use std::io::Read;
 use std::io::BufReader;
 use std::fs::File;
-// To use encoder.set()
+use std::ops::{SubAssign};
 use std::time::Instant;
 use na::{DVector, DMatrix, Real};
 use std::iter::IntoIterator;
@@ -12,6 +12,8 @@ use std::iter::IntoIterator;
 fn load_cifar(fname: &str) -> io::Result<(DMatrix<f32>, DVector<f32>)> {
     let f = File::open(fname)?;
 
+    // We hardcode these values because we know beforehand the dimensionalities
+    // of the CIFAR-10 training set
     let cfar_m = 10000usize;
     let cfar_n = 3072usize;
 
@@ -55,12 +57,6 @@ fn sigmoid<'a, I: IntoIterator<Item=&'a mut f32>>(it: I) {
     }
 }
 
-fn ln<T: Real>(x_mx: &DMatrix<T>) -> DMatrix<T> {
-    DMatrix::from_iterator(x_mx.shape().0,
-                           x_mx.shape().1,
-                           x_mx.iter().map(|v| v.ln()))
-}
-
 fn ln_v<T: Real>(x_mx: &DVector<T>) -> DVector<T> {
     DVector::from_iterator(x_mx.shape().0,
                            x_mx.iter().map(|v| v.ln()))
@@ -87,9 +83,48 @@ fn propagate(w: &DVector<f32>, b: f32, x_mx: &DMatrix<f32>,
     ((dw, db), cost)
 }
 
+fn optmise(w: &mut DVector<f32>, b: &mut f32, x_mx: &DMatrix<f32>,
+           y_mx: &DVector<f32>, num_iterations: usize,
+           learning_rate: f32)
+        -> f32 {
+    let mut cost = 0f32;
+    for _ in 0..num_iterations {
+        // Forward prop
+        let ((dw, db), c) = propagate(w, *b, x_mx, y_mx);
+        cost = c;
+
+        // Update weights
+        w.sub_assign(dw * learning_rate);
+        b.sub_assign(db * learning_rate);
+    }
+
+    cost
+}
+
+fn predict(w: &DVector<f32>, b: f32, x_mx: &DMatrix<f32>)
+        -> DVector<f32> {
+    // Compute activation value
+    let mut a_mx = (w.transpose() * x_mx).add_scalar(b).transpose();
+    sigmoid(&mut a_mx.iter_mut());
+
+    // Create vector Y of predictions
+    let y_mx = DVector::from_iterator(a_mx.shape().0, a_mx
+                .into_iter()
+                .map(|&x|
+                    if x < 0.5f32 {
+                        0f32
+                    }
+                    else {
+                        1f32
+                    }));
+
+    y_mx
+}
+
 fn main() {
     let data_file_name = "cifar-10-batches-bin/data_batch_1.bin";
 
+    // Load CIFAR data
     let start_t = Instant::now();
     let (mut x_mx, y_mx) = load_cifar(data_file_name)
         .expect("Error loading the data!");
@@ -97,17 +132,40 @@ fn main() {
     let elapsed = end_t.duration_since(start_t);
     println!("Time elapsed loading data : {:?}", elapsed);
 
+    // Standardise data
     let start_t = Instant::now();
     x_mx = standardise_rgb(&x_mx, 255f32);
     let end_t = Instant::now();
     let elapsed = end_t.duration_since(start_t);
     println!("Time elapsed standardising data : {:?}", elapsed);
 
+
+    // Setup weights and bias
+    let mut w = DVector::<f32>::zeros(x_mx.shape().0);
+    let mut b = 0f32;
+
+    // Optimise weights
     let start_t = Instant::now();
-    let w = DVector::<f32>::zeros(x_mx.shape().0);
-    let b = 2f32;
-    let ((dw, db), cost) = propagate(&w, b, &x_mx, &y_mx);
+    // Optimise w and b in place
+    let final_cost = optmise(&mut w, &mut b, &x_mx, &y_mx, 2000, 0.005f32);
     let end_t = Instant::now();
     let elapsed = end_t.duration_since(start_t);
-    println!("Time elapsed propagating data : {:?}", elapsed);
+    println!("Time elapsed optimising data : {:?}", elapsed);
+
+    // Predict
+    let start_t = Instant::now();
+    let y_hat = predict(&w, b, &x_mx);
+    let end_t = Instant::now();
+    let elapsed = end_t.duration_since(start_t);
+    println!("Time elapsed predicting: {:?}", elapsed);
+
+    let accuracy = 100f32 - (y_hat
+        .iter()
+        .zip(y_mx.iter())
+        .map(|(yh, y)| na::abs(&(yh - y)))
+        .sum::<f32>() / y_mx.nrows() as f32)
+        * 100f32;
+
+    println!("Final cost: {}", final_cost);
+    println!("Accuracy on training set: {}", accuracy);
 }
